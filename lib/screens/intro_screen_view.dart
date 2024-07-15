e1eda4f16e9aac652a7e83d3923e84defca6cfdd
Hery Rakotomanana <hrakotomanana@annexx.com>
2024-07-15 14:29:10 +0200

    v1.0.1

---
+++ lib/screens/intro_screen_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:intro_slider/intro_slider.dart';
+
+class IntroScreenView extends StatefulWidget {
+  const IntroScreenView({super.key});
+
+  @override
+  State<IntroScreenView> createState() => _IntroScreenViewState();
+}
+
+class _IntroScreenViewState extends State<IntroScreenView> {
+  List<ContentConfig> listContentConfig = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
